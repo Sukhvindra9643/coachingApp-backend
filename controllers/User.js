d53d405c23dffbb3304a5d56d637aff421e58f33
Sukhvindra9643 <20b1029.bca1@gitmgurgaon.com>
2023-03-08 16:05:02 +0530

    second commit

--- controllers/User.js
+++ controllers/User.js
@@ -8,7 +8,7 @@ export const register = async (req, res) => {
   try {
     
     const { name, email, password } = req.body;
-    // const avatar = req.files.avatar.tempFilePath;
+    const avatar = req.files.avatar.tempFilePath;
     let user = await User.findOne({ email });
     
     if (user) {
@@ -19,18 +19,18 @@ export const register = async (req, res) => {
     
     const otp = Math.floor(Math.random() * 1000000);
     
-    // const mycloud = await cloudinary.v2.uploader.upload(avatar);
+    const mycloud = await cloudinary.v2.uploader.upload(avatar);
     
-    // fs.rmSync("./tmp", { recursive: true });
+    fs.rmSync("./tmp", { recursive: true });
     
     user = await User.create({
       name,
       email,
       password,
-      // avatar: {
-        //   public_id: mycloud.public_id,
-        //   url: mycloud.secure_url,
-        // },
+      avatar: {
+          public_id: mycloud.public_id,
+          url: mycloud.secure_url,
+        },
         otp,
         otp_expiry: new Date(Date.now() + process.env.OTP_EXPIRE * 60 * 1000),
       });
